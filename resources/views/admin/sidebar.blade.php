dc394a12b2351cb08f88b724ccfeaa4d0162ba73
sinopaaalll <nrizqullah825@gmail.com>
2023-05-25 11:27:34 +0700

    merubah beberapa sidebar

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -1,20 +1,19 @@
     <div class="main-sidebar sidebar-style-2">
         <aside id="sidebar-wrapper">
           <div class="sidebar-brand">
-            <a href="index.html">Stisla</a>
+            <a href="{{ url('home') }}">Stisla</a>
           </div>
           <div class="sidebar-brand sidebar-brand-sm">
             <a href="index.html">St</a>
           </div>
+
           <ul class="sidebar-menu">
             <li class="menu-header">Dashboard</li>
-            <li class="dropdown active">
-              <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
-              <ul class="dropdown-menu">
-                <li class=active><a class="nav-link" href="index-0.html">General Dashboard</a></li>
-                <li><a class="nav-link" href="index.html">Ecommerce Dashboard</a></li>
-              </ul>
+            <li class="{{ Request::is('home') ? 'active' : '' }}">
+              <a class="nav-link active" href="{{ url('home') }}"><i class="fas fa-fire"></i> 
+                <span>Dashbaord</span></a>
             </li>
+            
             <li class="menu-header">Starter</li>
             <li class="dropdown">
               <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Layout</span></a>
@@ -24,7 +23,19 @@
                 <li><a class="nav-link" href="layout-top-navigation.html">Top Navigation</a></li>
               </ul>
             </li>
-            <li><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank Page</span></a></li>
+            <li>
+              <a class="nav-link" href="blank.html"><i class="far fa-square"></i> 
+                <span>Blank Page</span></a>
+            </li>
+
+            {{-- <li class="dropdown active">
+              <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+              <ul class="dropdown-menu">
+                <li class=active><a class="nav-link" href="index-0.html">General Dashboard</a></li>
+                <li><a class="nav-link" href="index.html">Ecommerce Dashboard</a></li>
+              </ul>
+            </li> --}}
+
           </ul>
 
         
