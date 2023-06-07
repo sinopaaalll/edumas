054d2d4d687eeaf559935a3b537f01bcbb6caab9
sinopaaalll <nrizqullah825@gmail.com>
2023-06-07 22:30:12 +0700

    dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Models\Pengaduan;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.pages.dashboard');
+
+        // Count jumlah dari tiap tiap databases
+        $count = [
+            'all' => Pengaduan::all()->count('id'),
+            'masuk' => Pengaduan::where('status','masuk')->count(),
+            'proses' => Pengaduan::where('status','proses')->count(),
+            'selesai' => Pengaduan::where('status','selesai')->count(),
+            'ditolak' => Pengaduan::where('status','ditolak')->count(),
+            'allUser' => User::all()->count('id'),
+            'admin' => User::where('role','admin')->count(),
+            'petugas' => User::where('role','petugas')->count(),
+            'masyarakat' => User::where('role','masyarakat')->count(),
+        ];
+
+        // Ambil data pengaduan dari database menggunakan Eloquent
+        $pengaduan = Pengaduan::selectRaw('DATE(tgl) as date, COUNT(*) as total')->groupBy('date')->get();
+
+        // Format data agar sesuai dengan format yang dibutuhkan oleh Chart.js
+        $labels = $pengaduan->pluck('date')->toArray();
+        $data = $pengaduan->pluck('total')->toArray();
+
+        $chartData = [
+            'labels' => $labels,
+            'datasets' => [
+                [
+                    'label' => 'Jumlah Pengaduan',
+                    'data' => $data,
+                    'backgroundColor' => '#6777ef',
+                    'borderColor' => '#6777ef',
+                    'borderWidth' => 3,
+                    'pointBackgroundColor' => '#ffffff',
+                    'pointRadius' => 4,
+                    'fill' => false,
+                ],
+            ],
+        ];
+        return view('admin.pages.dashboard', compact('chartData','count'));
     }
 }
--- resources/views/admin/pages/dashboard.blade.php
+++ resources/views/admin/pages/dashboard.blade.php
@@ -1,10 +1,10 @@
 @extends('admin.layouts.master')
 @section('content')
     <section class="section">
-        <div class="section-header">
-            <h1>Dashboard</h1>
-        </div>
         @if (auth()->user()->role == 'masyarakat')
+            <div class="section-header">
+                <h1>Pengaduan</h1>
+            </div>
             <div class="hero text-white hero-bg-image hero-bg-parallax" style="background-image: url('admin/assets/img/unsplash/andre-benz-1214056-unsplash.jpg');">
               <div class="hero-inner">
                 <h2>Welcome, {{ auth()->user()->name }}!</h2>
@@ -15,68 +15,131 @@
               </div>
             </div>
         @else
+        <div class="row">
+            <div class="col-lg-8 col-md-8 col-sm-12">
+              <div class="card card-statistic-2">
+                <div class="card-stats">
+                  <div class="card-stats-title">Pengaduan</div>
+                  <div class="card-stats-items">
+                    <div class="card-stats-item">
+                      <div class="card-stats-item-count">{{ $count['masuk'] }}</div>
+                      <div class="card-stats-item-label">Belum diproses</div>
+                    </div>
+                    <div class="card-stats-item">
+                      <div class="card-stats-item-count">{{ $count['proses'] }}</div>
+                      <div class="card-stats-item-label">Sedang diproses</div>
+                    </div>
+                    <div class="card-stats-item">
+                      <div class="card-stats-item-count">{{ $count['selesai'] }}</div>
+                      <div class="card-stats-item-label">Proses selesai</div>
+                    </div>
+                    <div class="card-stats-item">
+                      <div class="card-stats-item-count">{{ $count['ditolak'] }}</div>
+                      <div class="card-stats-item-label">Pengaduan ditolak</div>
+                    </div>
+                  </div>
+                </div>
+                <div class="card-icon shadow-primary bg-primary">
+                  <i class="fas fa-bullhorn"></i>
+                </div>
+                <div class="card-wrap">
+                  <div class="card-header">
+                    <h4>Total Pengaduan</h4>
+                  </div>
+                  <div class="card-body">
+                    {{ $count['all'] }}
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <div class="card card-statistic-2">
+                  <div class="card-stats">
+                    <div class="card-stats-title">Users</div>
+                    <div class="card-stats-items">
+                      <div class="card-stats-item">
+                        <div class="card-stats-item-count">{{ $count['admin'] }}</div>
+                        <div class="card-stats-item-label">Admin</div>
+                      </div>
+                      <div class="card-stats-item">
+                        <div class="card-stats-item-count">{{ $count['petugas'] }}</div>
+                        <div class="card-stats-item-label">Petugas</div>
+                      </div>
+                      <div class="card-stats-item">
+                        <div class="card-stats-item-count">{{ $count['masyarakat'] }}</div>
+                        <div class="card-stats-item-label">Masyarakat</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="card-icon shadow-primary bg-primary">
+                    <i class="fas fa-users"></i>
+                  </div>
+                  <div class="card-wrap">
+                    <div class="card-header">
+                      <h4>Total User</h4>
+                    </div>
+                    <div class="card-body">
+                        {{ $count['allUser'] }}
+                    </div>
+                  </div>
+                </div>
+              </div>
+          </div>
+
+            
+            
             <div class="row">
-                <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-                    <div class="card card-statistic-1">
-                        <div class="card-icon bg-primary">
-                            <i class="fas fa-bullhorn"></i>
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Statistik Jumlah Pengaduan</h4>
                         </div>
-                        <div class="card-wrap">
-                            <div class="card-header">
-                                <h4>Semua Pengaduan</h4>
-                            </div>
-                            <div class="card-body">
-                        
-                            </div>
+                        <div class="card-body">
+                            <canvas id="lineChart"></canvas>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-                    <div class="card card-statistic-1">
-                        <div class="card-icon bg-danger">
-                            <i class="fas fa-exclamation-triangle"></i>
-                        </div>
-                        <div class="card-wrap">
-                            <div class="card-header">
-                            <h4>Belum di Proses</h4>
-                            </div>
-                            <div class="card-body">
-                                
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-                    <div class="card card-statistic-1">
-                        <div class="card-icon bg-warning">
-                            <i class="fas fa-exchange-alt"></i>
-                        </div>
-                        <div class="card-wrap">
-                            <div class="card-header">
-                            <h4>Sedang di Proses</h4>
-                            </div>
-                            <div class="card-body">
-                        
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-                    <div class="card card-statistic-1">
-                        <div class="card-icon bg-success">
-                            <i class="fas fa-check-circle"></i>
-                        </div>
-                        <div class="card-wrap">
-                            <div class="card-header">
-                            <h4>Selesai</h4>
-                            </div>
-                            <div class="card-body">
-                        
-                            </div>
-                        </div>
-                    </div>
-                </div>                  
             </div>
         @endif
     </section>
+    @push('script')
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            var chartData = @json($chartData);
+
+            var ctx = document.getElementById('lineChart').getContext('2d');
+            var lineChart = new Chart(ctx, {
+                type: 'line',
+                data: chartData,
+                options: {
+                    legend: {
+                    display: false
+                    },
+                    scales: {
+                        yAxes: [{
+                            gridLines: {
+                                drawBorder: false,
+                                color: '#f2f2f2',
+                            },
+                            ticks: {
+                                beginAtZero: true,
+                                stepSize: 1
+                            }
+                        }],
+                        xAxes: [{
+                            ticks: {
+                                display: true
+                            },
+                            gridLines: {
+                                display: false
+                            }
+                        }]
+                    },
+                }
+            });
+        });
+      </script>
+      <!-- End Bar CHart -->
+    @endpush
 @endsection
